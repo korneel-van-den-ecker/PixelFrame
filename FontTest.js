fd41bdc3e6cdb83b86e71a25f591a68d0b66f795
koranus <korneel_van_den_ecker@hotmail.com>
2019-03-27 21:03:32 +0000

    al iets verder

--- FontTest.js
+++ FontTest.js
@@ -2,3 +2,5 @@ var Letters = require('./letters.js')
 
 var letters = new Letters();
 console.log(letters.breedte);
+
+var tets = letters.VerkrijgPixelCharacter('a');
--- letters.js
+++ letters.js
@@ -11,6 +11,33 @@ module.exports = class Letter{
         console.log(this.font.CharSet);
         this.breedte = this.font.Size.Dx;
         this.hoogte = this.font.Size.Dy;
-    }
+    };
+
+    VerkrijgPixelCharacter(karakter){
+        //Pixellijst inittialiseren
+        var pixellijst = new Array(this.breedte);
+        for(var i = 0; i < this.hoogte; i++){
+            pixellijst[i] = new Array(this.hoogte);
+        };
+
+        //if(this.font.CharSet.includes(karakter)){
+            // Het karakter opzoeken in de json en de bitmap setten
+            var bitmap = this.font.CharSet[this.font.CharSet.find(i => i.Character == karakter)].Bitmap
+            console.log(bitmap)
+        //};
+
+        //De bitmap omzetten naar een lijst van pixels
+        for(var i = 0; i < this.hoogte; i++){
+            var str = bitmap[i];
+            for(j = 0; j < this.breedte; j++){
+                if(str[j] == 0){
+                    pixellijst[i,j] = 1;
+                }
+            };
+        };
+
+        return pixellijst;
+
+    };
 };
 
